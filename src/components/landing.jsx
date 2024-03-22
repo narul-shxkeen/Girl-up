7ae306fe821815e015897c8bb52cd06650998576
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-03-22 12:27:55 +0530

    Landing page updated

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 
 function Landing(){
-    return <div className="bg-black w-[99vw] h-[90vh] flex justify-between">
-        <div className="w-1/2"></div>
-        <div className="w-1/2 py-32 flex flex-col pr-8">
-            <h1 className="text-white inika-bold pb-4 tracking-wide">GIRLUP UJJIWAN</h1>
-            <p className="inika-regular">
+    return <div className="bg-black w-[99vw] h-[90vh] flex justify-center">
+        <div className="flex flex-col backback w-11/12 rounded-[65px] h-[80vh] mt-8 items-center">
+            <h1 className="text-3xl inika-bold pb-4 tracking-wide mt-4">GIRLUP UJJIWAN</h1>
+            {/* <p className="inika-regular">
 
             Girl Up is a girl-centered leadership development 
             initiative, focusing on equity for girls 
             and women in spaces where they are unheard or underrepresented. We believe when girls and 
             women are in positions of influence, they work to create a more just and equitable world for everyone.
 
-            </p>
+            </p> */}
             </div>
             </div>
 }
