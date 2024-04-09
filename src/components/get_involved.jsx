0ab627ec0b4175a1ea301fb490470ab8d561f0bf
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-04-09 15:00:27 +0530

    design responsiveness error corrected

--- src/components/get_involved.jsx
+++ src/components/get_involved.jsx
@@ -5,7 +5,7 @@ const Get_involved = () => {
    <div className='h-full'>
         <div className="flex flex-col w-[100%] rounded-[65px] h-full overflow-hidden z-20">
             <h1 className="text-6xl inika-bold pb-4 tracking-wide mt-4 z-20 tracking-widest text-center">Get Involved</h1>
-            <div className='flex items-center justify-center sm:justify-start w-full mx-0 px-20'>
+            <div className='flex items-center justify-center sm:justify-start w-full mx-0 sm:px-20'>
             <div className="flex w-3/4 py-11 sm:items-center sm:justify-start">
                 <img src="/images/Slice 1.png" className="min-w-[150px] sm:w-1/3 rounded-full"/>
                 </div>
