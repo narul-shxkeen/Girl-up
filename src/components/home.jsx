c5fd80a75acb07e477cd0440cafca9188b8da3b9
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-04-18 17:23:54 +0530

    fixed width of landing image on larger screens

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         </div>
         <div className=" relative flex flex-col w-[100%] h-full sm:items-center justify-center overflow-hidden z-20">
             <h1 className="text-3xl sm:text-4xl md:text-6xl inika-bold sm:pb-4 tracking-wide mt-1 sm:mt-4 z-20 text-center">GIRL UP UJJIWAN</h1>
-            <div className="xs:mx-2 sm:mx-16 text-center text-md sm:text-xl text-[#502E2A] z-20 leading-relaxed">
+            <div className="xs:px-2 sm:px-16 text-center text-md sm:text-xl text-[#502E2A] z-20 leading-relaxed">
                 <p className="inika-regular">
                 GirUp Ujjiwan is a women centered organisation aiming to foster leadership and empowerment initiatives for the women of Haryana. Focussing on the financial literacy of girls and women, we look to create a space where they are heard and given all the necessary tools and education they need to become financially independent. Once provided with agency and confidence, we believe when women have the power to change the world; which is exactly what we strive to achieve!
                 </p>
