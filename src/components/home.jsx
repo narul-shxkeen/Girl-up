5cbfd272f04defa61312dbc9010dfaa073fc2af4
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-05-07 12:37:55 +0530

    Finally the landing image is responsivegit add .

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
    <div className='h-[100vh] md:h-full relative overflow-hidden'>
         <div className="absolute sm:w-[110%] z-[-5]">
-            <img className="xs:h-[80vh] sm:w-[100%] object-fill" src="/images/home2.png"/>
+            <img className="xs:h-[80vh] sm:w-[100%] object-cover" src="/images/home2.png"/>
         </div>
         <div className=" relative flex flex-col w-[100%] h-full sm:items-center justify-center overflow-hidden z-20">
             <h1 className="text-3xl sm:text-4xl md:text-6xl inika-bold sm:pb-4 tracking-wide mt-1 sm:mt-4 z-20 text-center">GIRL UP UJJIWAN</h1>
