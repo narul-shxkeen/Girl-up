fdf84b3cc92fe718674e6249dfbc94a01e29d71a
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-05-19 11:37:04 +0530

    about page added

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 
 function Template(){
-    return <div>
-        <h1>Helllo Fuckers</h1>
+
+    return <div className=""> 
+    <div className="bg-[#E3D5CA] flex flex-col items-center h-[100vh] overflow-y-scroll">
+        <h1 className="sen-regular mt-3 text-7xl mb-3">About Us</h1>
+        <div className="relative h-[150vh]">
+            <img src="/images/home2.png" className="w-[100vw] h-auto z-10 object-cover"/>
+            <div className="bg-[#D9D9D9] rounded-[65px] z-30 mx-40 mb-40 my-12 absolute top-[10px] px-5 py-12  ">
+                <p className="poppins-regular text-xl text-black">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste vitae nobis eligendi eum veniam sint fugit commodi facilis repellat blanditiis veritatis voluptates magnam sed optio deserunt dolorum labore, accusamus explicabo! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, tempora! Necessitatibus debitis, officiis incidunt cum enim sed. Obcaecati, perspiciatis, cum recusandae nemo similique fuga dolore incidunt autem atque voluptate in?Maxime a alias dolor saepe qui aut labore ad cum, velit nesciunt placeat accusantium aliquid porro non assumenda, officia recusandae beatae molestiae.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sunt dolor dolorum beatae! Quam nobis quos odio maiores ducimus eaque, ut vitae, impedit enim, aperiam assumenda molestias. Delectus, dolore dignissimos.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, veniam! Reprehenderit ipsum eum at alias iste aut nemo animi, repellat ab neque veniam impedit fugit unde sit fugiat! Hic, dignissimos?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo quidem odio molestias error placeat, quisquam expedita eveniet esse cum at? Inventore fuga doloribus quisquam culpa vero ea labore, corrupti dignissimos!
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsam blanditiis nulla tempora voluptates error, corrupti vel ullam, earum consequatur totam odit eius cumque velit modi eaque, deleniti sequi. Cupiditate?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ipsum reiciendis ad et magnam facere soluta amet iusto aspernatur laborum. Dolore doloremque quam, cumque expedita harum animi saepe sit voluptatibus.</p>
+            </div>
+        </div>
+    </div>
     </div>
 }
 
