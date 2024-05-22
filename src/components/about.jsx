1a503d5016ee9f7862b6d54e23a61c8ee8a86cfe
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-05-22 13:21:07 +0530

    bottom margin of about us adjusted

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,7 +7,7 @@ function Template(){
         <h1 className="sen-regular mt-3 text-7xl mb-3 text-center">About Us</h1>
         <div className="relative flex justify-center">
             <img src="/images/home2.png" className="hidden md:block h-[150vh] z-0 object-cover"/>
-            <div className="bg-[#D9D9D9] rounded-none sm:rounded-[65px] z-10 my-10 flex absolute top-[10px] px-5 pt-12 pb-40 sm:pt-12 sm:pb-12 w-[100vw] sm:w-2/3">
+            <div className="bg-[#D9D9D9] rounded-none sm:rounded-[65px] z-10 my-10 flex absolute top-[10px] px-5 pt-12 pb-52 sm:pt-12 sm:pb-12 w-[100vw] sm:w-2/3">
                 <p className="poppins-regular text-xl text-black">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste vitae nobis eligendi eum veniam sint fugit commodi facilis repellat blanditiis veritatis voluptates magnam sed optio deserunt dolorum labore, accusamus explicabo! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, tempora! Necessitatibus debitis, officiis incidunt cum enim sed. Obcaecati, perspiciatis, cum recusandae nemo similique fuga dolore incidunt autem atque voluptate in?Maxime a alias dolor saepe qui aut labore ad cum, velit nesciunt placeat accusantium aliquid porro non assumenda, officia recusandae beatae molestiae.
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sunt dolor dolorum beatae! Quam nobis quos odio maiores ducimus eaque, ut vitae, impedit enim, aperiam assumenda molestias. Delectus, dolore dignissimos.
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, veniam! Reprehenderit ipsum eum at alias iste aut nemo animi, repellat ab neque veniam impedit fugit unde sit fugiat! Hic, dignissimos?
