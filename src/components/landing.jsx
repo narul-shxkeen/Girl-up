a0ee683b6ac75d1ce869a99b2fbeb04414dd2b55
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-05-25 07:25:39 +0530

    more stylization for small screens

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -7,7 +7,7 @@ import "../App.css"
 function Landing(){
     return <div className="bg-[#EADCCB] w-[100vw] h-[90vh] flex justify-center z-10 overflow-hidden">
   
-        <div className="bg-[#F5F5F5] flex flex-col w-11/12 rounded-[65px] h-[80vh] mt-8 items-center overflow-hidden justify-center z-0">
+        <div className="bg-[#F5F5F5] flex flex-col w-[100vw] sm:w-11/12 rounded-none sm:rounded-[65px] h-[80vh] mt-8 items-center overflow-hidden justify-center z-0">
             <div className="overflow-y-scroll h-full w-full">
                 <Home className=""/>
                 {/* <Get_involved/> */}
