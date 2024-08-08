a69fe7e011146ecc7d988ba93d108b127734f9a6
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-08-08 18:27:10 +0530

    fixing mobile responsiveness

--- src/assets/eventcard.jsx
+++ src/assets/eventcard.jsx
@@ -7,10 +7,10 @@ const EventCard = ({ eventName, date, link }) => {
 
     return(
         <div className="flex items-center justify-center md:items-left">
-        <div className="bg-[#D9D9D9] md:rounded-[20px] sm:rounded-[65px] z-10 my-10 px-5 pt-12 pb-12 sm:pt-12 sm:pb-12 w-[90vw] sm:w-[450px] sm:h-[250px]" >
+        <div className="bg-[#D9D9D9] md:rounded-[20px] sm:rounded-[65px] z-10 my-10 px-5 pt-12 pb-12 sm:pt-12 sm:pb-12 w-[70vw] h-auto sm:w-[450px] sm:!h-[250px]" >
             <p className="sen-regular text-[#502E2A] font-bold text-3xl"> {eventName} </p>
             <p className="sen-regular text-[#502E2A] text-2xl"> {date} </p>
-            <div className="bg-black md:rounded-[20px] sm:rounded-[65px] p-3 sm:pt-12 sm:pb-12 sm:w-2/3 mt-4 md:ml-48 sm:ml-28" style={{ width: 'fit-content'}}>
+            <div className="bg-black rounded-lg p-3 sm:pt-12 sm:pb-12 sm:w-2/3 mt-4 md:ml-48 sm:ml-28" style={{ width: 'fit-content'}}>
             <Link to={link} className="poppins-regular text-xl text-white">  learn more </Link>
             </div>
         </div>
