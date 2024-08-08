ac3654bbf22dcc51cac3fa77264e91ba5efbf21b
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-08-08 18:19:54 +0530

    made the updates responsive too

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -9,7 +9,7 @@ const Project = () => {
 
     const ProjectCard = ({ link, image, altText, title, handleClick }) => {
         return (
-            <div className="w-full sm:w-3/4 py-11 flex justify-center">
+            <div className="w-[80vw] sm:w-3/4 py-11 flex justify-center">
                 <Link to={link}>
                     <button className='flex flex-col w-[250px] gap-3 bg-[#D9D9D9] justify-center items-center min-w-40' onClick={handleClick}>
                         <img src={image} alt={altText} className='w-full h-auto' />
@@ -24,7 +24,7 @@ const Project = () => {
         <div className='h-full'>
             <div className="flex flex-col w-[100%] rounded-[65px] z-20 items-center">
                 <h1 className="text-6xl inika-bold pb-4 mt-4 z-20 tracking-widest text-center">Projects</h1>
-                <div className='grid grid-cols-3 items-center justify-items-center w-[80vw] h-auto px-5 gap-3'>
+                <div className='flex flex-col sm:grid sm:grid-cols-3 items-center justify-items-center w-[80vw] h-auto sm:px-5 sm:gap-3'>
                 <ProjectCard link="/first" image="/images/first_project/1.png" altText="Image of budgeting workshop" title="Budgeting" handleClick={handleClick} />
                 
                 <ProjectCard link="/second" image="/images/second/1.jpg" altText="Image of loan workshop" title="Loans" handleClick={handleClick} />
