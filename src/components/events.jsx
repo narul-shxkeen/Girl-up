95cc09695ef5b378e9f32c6c012126335fd14b8e
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-08-09 10:48:28 +0530

    added upcoming events section

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -9,11 +9,14 @@ function EventMain(){
     <div className="bg-[#E3D5CA] h-[100vh] overflow-y-scroll">
         <h1 className="sen-regular mt-5 sm:mt-3 text-7xl mb-1 sm:mb-3 text-center"> EVENTS</h1>
         
-        <p className="font-bold text-[#502E2A] drop-shadow-[0_4px_4px_rgba(0, 0, 0, 0.25)] sen-regular mt-5 sm:mt-3 text-5xl mb-1 sm:mb-3 text-center"> Recent Events: </p>
+        <p className="font-bold text-[#502E2A] drop-shadow-[0_4px_4px_rgba(0, 0, 0, 0.25)] sen-regular mt-5 sm:mt-3 text-5xl mb-1 sm:mb-3 text-center"> Upcoming Events: </p>
        <div className="grid md:grid-cols-2 gap-4 justify-items-center px-8 py-20">
-        <EventCard eventName="First Event: Budgeting" date = "30th March, 2024" link= "/first"/>
         <EventCard eventName="Second Event: Loans" date = "" link= "/second"/>
 <EventCard eventName="Third Event: Self Help Groups" date = "" link= "/third"/>
+        </div>
+        <p className="font-bold text-[#502E2A] drop-shadow-[0_4px_4px_rgba(0, 0, 0, 0.25)] sen-regular mt-5 sm:mt-3 text-5xl mb-1 sm:mb-3 text-center"> Past Events: </p>
+       <div className="grid md:grid-cols-2 gap-4 justify-items-center px-8 py-20">
+        <EventCard eventName="First Event: Budgeting" date = "30th March, 2024" link= "/first"/>
         </div>
         </div></div>
     )    
